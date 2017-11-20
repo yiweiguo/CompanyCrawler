b1969d0ef05be7ee1144c014e8d25a86ddc278e4
yiweiguo <yiweiguo0301@gmail.com>
2017-11-20 16:11:49 -0500

    Crawl the information using Fullcontact api wrapper

--- crawler.py
+++ crawler.py
@@ -1,4 +1,32 @@
 from openpyxl import load_workbook
+from FullContact import FullContact
+import json
+import requests
+
+ACCESS_TOKEN = '58188b712e32cd16'
+
+class FullContact(object):
+	def __init__(self, api_key):
+		self.api_key = api_key
+		self.base_url = 'https://api.fullcontact.com/v2/'
+		self.get_endpoints = {
+			'person': 'person.json',
+			'company_search': 'company/search.json'
+		}
+		self.post_endpoints = {
+            'batch': 'batch.json'
+        }
+		for endpoint in self.get_endpoints:
+			# method that will invoke the self.api_get
+			method = lambda endpoint = endpoint, **kwargs: self.api_get(endpoint, **kwargs)
+			# set the end_point to be a method() callable
+			setattr(self, endpoint, method)
+
+
+	def api_get(self, endpoint, **kwargs):
+		headers = {'X-FullContact-APIKey': self.api_key}
+		endpoint = self.base_url + self.get_endpoints[endpoint]
+		return requests.get(endpoint, params=kwargs, headers=headers)
 
 def load_data(filename):
 	'''
@@ -19,12 +47,31 @@ def load_data(filename):
 	assert(len(names) <= len(cells_dict))
 	return names
 
+def get_info_dict(names):
+	'''
+	get the company name and info hash 
+	parameters:
+	names: names of the company in a list
+	return:
+	{company_name: json file of the company}
+	'''
+	info_dict = {}
+	fc = FullContact(ACCESS_TOKEN)
+	for name in names: 
+		print(name)
+		# calls are made in a small window
+		info_dict[name] = fc.company_search(companyName=name)
+		sleep(0.5)
+	return info_dict
 
+def main():	
+	filename = 'Financial_company _UNlist.xlsx'
 
+	names = load_data(filename)
+	info_dict = get_info_dict(names)
+	print(info_dict['LUMA Capital, LLC'])
 
+if __name__ == "__main__":
+    main()
 
-filename = 'Financial_company _UNlist.xlsx'
-
-names = load_data(filename)
-for name in names:
-	print(name)
+	
